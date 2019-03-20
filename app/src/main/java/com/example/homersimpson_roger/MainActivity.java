8106a8735834351c3337c0c678955e3394b6982b
RogerMiralles <miralles.monfort@gmail.com>
2019-03-20 11:03:46 +0100

    Apartat b.

--- app/src/main/java/com/example/homersimpson_roger/MainActivity.java
+++ app/src/main/java/com/example/homersimpson_roger/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.homersimpson_roger;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView animacioTitol;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        animacioTitol= findViewById(R.id.titol);
+        AnimationDrawable anim=(AnimationDrawable) animacioTitol.getDrawable();
+        anim.start();
     }
 }
